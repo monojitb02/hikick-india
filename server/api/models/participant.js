3c48b25b8f57eeb9043f0c3de136ea3b3fc9ca02
monojitb02 <monojitb02@gmail.com>
2014-11-30 19:31:26 +0530

    added some schema and db backup

---
+++ server/api/models/participant.js
@@ -0,0 +1,78 @@
+'use strict';
+
+var mongoose = require('../../lib').mongoose,
+    Schema = mongoose.Schema,
+    participantSchema;
+
+participantSchema = new Schema({
+    participantId: {
+        type: Number,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    instructor: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    state: {
+        type: String,
+        required: false
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    clubName: {
+        type: String,
+        required: false
+    },
+    dob: {
+        type: Date,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    choiceOfEvents: {
+        kata: {
+            type: Boolean,
+            default: false
+        },
+        kumite: {
+            type: Boolean,
+            default: false
+        },
+        weapons: {
+            type: Boolean,
+            default: false
+        }
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    profilePictureFile: {
+        type: String,
+        required: false
+    }
+}, {
+    versionKey: false
+});
+participantSchema.index({
+    participantId: 1
+}, {
+    unique: true
+});
+module.exports = mongoose.model('participant', participantSchema);
--- server/api/models/user.js
+++ server/api/models/user.js
@@ -6,141 +6,55 @@
 var lib = require('../../lib'),
     Schema = lib.mongoose.Schema,
     userSchema = new Schema({
-        companyProfile: {
-            empId: {
-                type: String,
-                required: true
-            },
-            company: {
-                type: Schema.Types.ObjectId,
-                ref: 'company',
-                required: true
-            },
-            attendanceId: {
-                type: Number,
-                required: true
-            },
-            role: {
-                type: Schema.Types.ObjectId,
-                ref: 'role',
-                required: true
-            },
-            DOJ: {
-                type: Date,
-                required: true
-            },
-            DOB: {
-                type: Date,
-                required: true
-            },
-            name: {
-                first: {
-                    type: String,
-                    required: true
-                },
-                middle: {
-                    type: String
-                },
-                last: {
-                    type: String,
-                    required: true
-                }
-            },
-            email: {
-                type: String,
-                unique: true,
-                required: true
-            },
-            gender: {
-                type: String,
-                required: true
-            },
-            designation: {
-                type: Schema.Types.ObjectId,
-                ref: 'designation',
-                required: true
-            },
-            manager: {
-                type: Schema.Types.ObjectId,
-                ref: 'user'
-            },
-            panId: {
-                type: String,
-                required: true
-            },
-            CTC: {
-                type: Number,
-                required: true
-            },
-            isActive: {
+        role: {
+            registerCandidate: {
                 type: Boolean,
-                default: true
+                'default': false
+            },
+            sheduleEvent: {
+                type: Boolean,
+                'default': false
+            },
+            dojoMat1: {
+                type: Boolean,
+                'default': false
+            },
+            dojoMat2: {
+                type: Boolean,
+                'default': false
+            },
+            dojoMat3: {
+                type: Boolean,
+                'default': false
+            },
+            dojoMat4: {
+                type: Boolean,
+                'default': false
+            },
+            gameHistory: {
+                type: Boolean,
+                'default': false
             }
         },
-        personalProfile: {
-            password: {
-                type: String,
-                required: true
-            },
-            photoUrl: {
-                type: String
-            },
-            contactNumbers: [{
-                type: String,
-                required: true
-            }],
-            bioData: {
-                type: String
-            },
-            permanentAddress: {
-                fullAddress: {
-                    type: String,
-                    required: true
-                },
-                PIN: {
-                    type: Number,
-                    required: true
-                },
-                city: {
-                    type: String,
-                    required: true
-                },
-                state: {
-                    type: String,
-                    required: true
-                },
-                country: {
-                    type: String,
-                    required: true
-                }
-            },
-            currentAddress: {
-                fullAddress: {
-                    type: String
-                },
-                PIN: {
-                    type: Number
-                },
-                city: {
-                    type: String
-                },
-                state: {
-                    type: String
-                },
-                country: {
-                    type: String
-                }
-            }
+        name: {
+            type: String,
+            required: true
         },
-        lastLogin: {
-            date: {
-                type: Date
-            },
-            ip: {
-                type: String
-            }
+        email: {
+            type: String,
+            unique: true,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
         }
     }, {
         versionKey: false
     });
+userSchema.index({
+    email: 1
+}, {
+    unique: true
+});
 module.exports = lib.mongoose.model("user", userSchema)
