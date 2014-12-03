4999a8f06ff6ac20a73e1d0a8f9dc7241ededaab
Asis Datta <dattaasis.1993@gmail.com>
2014-12-03 23:34:40 +0530

    login issue

--- app/application/controllers/loginCtrl.js
+++ app/application/controllers/loginCtrl.js
@@ -2,28 +2,19 @@
 
 var utility = require('../../util');
 
-module.exports = function($scope, $state, $location) {
+module.exports = function($scope, $state) {
+
+    var id = utility.getCookie('uid');
+    if (id) {
+        $state.go('app.home');
+    }
     //dummy login
-    $scope.uname='asis';
-    $scope.password='asis';
-    
+    $scope.uname = 'asis';
+    $scope.password = 'asis';
+
     $scope.login = function() {
         utility.setCookie('uid', $scope.uname);
         $state.go('app.home');
         //$location.path('/home');
     };
-    jQuery.validator.setDefaults({
-  		debug: true,
-  		success: "valid"
-	});
-    jQuery("#login_form").validate({
-        rules: {
-            uname: {
-                required: true
-            },
-            password:{
-            	required:true
-            }
-        }
-    });
 };
